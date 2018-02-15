51771704222231204b00d60a5baef9ccf542e94a
Gokatta <hieuht300894@gmail.com>
2018-02-15 07:06:54 +0700

    1

--- Mega_Joystick_v2/Mega_Joystick_v2.ino
+++ Mega_Joystick_v2/Mega_Joystick_v2.ino
@@ -3,17 +3,19 @@
 #define pin_Start  22
 #define pin_Step 2
 #define pin_Total 15
-#define def_ValueStepLeft 400
-#define def_ValueStepRight 612
+#define def_ValueStepLeft 399
+#define def_ValueStepRight 611
 #define def_ValueMin 0
-#define def_ValueCenter 512
-#define def_ValueMax 1024
+#define def_ValueCenter 511
+#define def_ValueMax 1023
 #define pin_Reset 42
 #define def_Normal 0
 #define def_Race 1
-#define pin_X_Rotation A1
-#define pin_Y_Rotation A2
-#define pin_Z_Rotation A3
+#define pin_X_Axist A1
+#define pin_Y_Axist A2
+#define pin_X_Rotation A3
+#define pin_Y_Rotation A4
+#define pin_Z_Rotation A5
 
 /*Default: 1, 0: Normal, 1: Race*/
 int mode = 1;
@@ -46,9 +48,24 @@ void setup()
 void loop()
 {
   // Always be getting fresh data
-
   if (is_Start)
   {
+    if (digitalRead(pin_Reset) != prev_Status)
+    {
+      prev_Status = !prev_Status;
+      if (!prev_Status)
+      {
+        if (mode == def_Normal)
+          mode = def_Race;
+        else  if (mode == def_Race)
+          mode = def_Normal;
+
+        is_Start = false;
+        setupValues();
+        is_Start = true;
+      }
+    }
+
     megaJoyControllerData_t controllerData = getControllerData();
     setControllerData(controllerData);
   }
@@ -71,17 +88,9 @@ void setupPins(void)
 }
 
 void setupValues() {
-  A1_ValueCenter =  analogRead(pin_X_Rotation);
+  A1_ValueCenter =  analogRead(pin_X_Axist);
   A1_ValueError = def_ValueCenter - A1_ValueCenter;
   A1_ValueError_2 = def_ValueCenter - map(A1_ValueCenter + A1_ValueError, def_ValueStepLeft, def_ValueStepRight, def_ValueMin, def_ValueMax);
-
-  A2_ValueCenter =  analogRead(pin_Y_Rotation);
-  A2_ValueError = def_ValueCenter - A2_ValueCenter;
-  A2_ValueError_2 = def_ValueCenter - map(A2_ValueCenter + A2_ValueError, def_ValueStepLeft, def_ValueStepRight, def_ValueMin, def_ValueMax);
-
-  A3_ValueCenter =  analogRead(pin_Z_Rotation);
-  A3_ValueError = def_ValueCenter - A3_ValueCenter;
-  A3_ValueError_2 = def_ValueCenter - map(A3_ValueCenter + A3_ValueError, def_ValueStepLeft, def_ValueStepRight, def_ValueMin, def_ValueMax);
 }
 
 megaJoyControllerData_t getControllerData(void) {
@@ -101,21 +110,7 @@ megaJoyControllerData_t getControllerData(void) {
   {
     if (pin_Current == pin_Reset)
     {
-      if (digitalRead(pin_Reset) != prev_Status)
-      {
-        prev_Status = !prev_Status;
-        if (!prev_Status)
-        {
-          if (mode == def_Normal)
-            mode = def_Race;
-          else  if (mode == def_Race)
-            mode = def_Normal;
 
-          is_Start = false;
-          setupValues();
-          is_Start = true;
-        }
-      }
     }
     else
     {
@@ -138,63 +133,29 @@ megaJoyControllerData_t getControllerData(void) {
   //  controllerData.analogAxisArray[4] = analogRead(A2);// Y Rotation
   //  controllerData.analogAxisArray[5] = analogRead(A3);// Z Rotation
 
-  controllerData.analogAxisArray[0] = def_ValueCenter;
+  controllerData.analogAxisArray[0] = getValueXAxist();
   controllerData.analogAxisArray[1] = def_ValueCenter;
   controllerData.analogAxisArray[2] = def_ValueCenter;  // Z Axis
   controllerData.analogAxisArray[3] = def_ValueCenter;  // X Rotation
   controllerData.analogAxisArray[4] = def_ValueCenter;// Y Rotation
-  controllerData.analogAxisArray[5] = getValuePinA3();// Z Rotation
+  controllerData.analogAxisArray[5] = def_ValueCenter;// Z Rotation
 
   // And return the data!
   return controllerData;
 }
 
-int getValuePinA1() {
+int getValueXAxist() {
   int val = def_ValueCenter;
   if (mode == def_Normal)
-    val = analogRead(pin_X_Rotation) + A1_ValueError;
+    val = analogRead(pin_X_Axist) + A1_ValueError;
   else if (mode == def_Race) {
-    int temp = analogRead(pin_X_Rotation) + A1_ValueError;
+    int temp = analogRead(pin_X_Axist) + A1_ValueError;
     if (temp < def_ValueStepLeft)
       val = def_ValueMin;
     else if (temp > def_ValueStepRight)
       val = def_ValueMax;
     else
       val = map(temp, def_ValueStepLeft, def_ValueStepRight, def_ValueMin, def_ValueMax) + A1_ValueError_2;
-  }
-
-  return val;
-}
-
-int getValuePinA2() {
-  int val = def_ValueCenter;
-  if (mode == def_Normal)
-    val = analogRead(pin_Y_Rotation) + A2_ValueError;
-  else if (mode == def_Race) {
-    int temp = analogRead(pin_Y_Rotation) + A2_ValueError;
-    if (temp < def_ValueStepLeft)
-      val = def_ValueMin;
-    else if (temp > def_ValueStepRight)
-      val = def_ValueMax;
-    else
-      val = map(temp, def_ValueStepLeft, def_ValueStepRight, def_ValueMin, def_ValueMax) + A2_ValueError_2;
-  }
-
-  return val;
-}
-
-int getValuePinA3() {
-  int val = def_ValueCenter;
-  if (mode == def_Normal)
-    val = analogRead(pin_Z_Rotation) + A3_ValueError;
-  else if (mode == def_Race) {
-    int temp = analogRead(pin_Z_Rotation) + A3_ValueError;
-    if (temp < def_ValueStepLeft)
-      val = def_ValueMin;
-    else if (temp > def_ValueStepRight)
-      val = def_ValueMax;
-    else
-      val = map(temp, def_ValueStepLeft, def_ValueStepRight, def_ValueMin, def_ValueMax) + A3_ValueError_2;
   }
 
   return val;
